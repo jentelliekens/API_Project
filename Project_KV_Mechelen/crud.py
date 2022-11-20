c932b5c97c3369c49a40d7220beb7bd8baca5be2
Jentel Liekens <r0835618@student.thomasmore.be>
2022-11-20 23:31:55 +0100

    README.md update 4

--- Project_KV_Mechelen/crud.py
+++ Project_KV_Mechelen/crud.py
@@ -32,17 +32,3 @@ def create_speler(db: Session, player: schemas.SpelerCreate):
     db.refresh(db_spelers)
     return db_spelers
 
-
-
-
-
-# def get_items(db: Session, skip: int = 0, limit: int = 100):
-#     return db.query(models.Item).offset(skip).limit(limit).all()
-#
-#
-# def create_user_item(db: Session, item: schemas.ItemCreate, user_id: int):
-#     db_item = models.Item(**item.dict(), owner_id=user_id)
-#     db.add(db_item)
-#     db.commit()
-#     db.refresh(db_item)
-#     return db_item
